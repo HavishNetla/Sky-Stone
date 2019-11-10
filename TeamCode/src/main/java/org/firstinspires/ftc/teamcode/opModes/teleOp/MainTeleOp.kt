fb9116c4cc3d9656fec7d994d5fe00fe2233a825
Havish Netla <netlahavish@gmail.com>
2019-11-10 12:04:59 -0500

    Debug Impl

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/teleOp/MainTeleOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/teleOp/MainTeleOp.kt
@@ -2,16 +2,20 @@ package org.firstinspires.ftc.teamcode.opModes.teleOp
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import org.firstinspires.ftc.teamcode.computerDebuging.ComputerDebugging
 import org.firstinspires.ftc.teamcode.subsystems.Robot
 import org.firstinspires.ftc.teamcode.util.Vector2d
 
 @TeleOp(name = "Mecanum Drive", group = "T")
 class MainTeleOp : OpMode() {
     private lateinit var robot: Robot
-    private var count: Int = 0
+    private lateinit var computerDebugging: ComputerDebugging
+
     override fun init() {
         robot = Robot(this, this.telemetry)
         robot.start()
+
+        computerDebugging = ComputerDebugging()
     }
 
     override fun loop() {
@@ -21,5 +25,8 @@ class MainTeleOp : OpMode() {
 
         telemetry.addData("Position", robot.drive.position)
 
+        ComputerDebugging.sendRobotLocation(robot.drive.position)
+        ComputerDebugging.sendNoClear(robot.drive.position.pos())
+        ComputerDebugging.sendPacket()
     }
 }
