acef383da30bf91c1ff1351d80f8e592b7e4ec4a
Havish Netla <netlahavish@gmail.com>
2019-12-22 12:08:52 -0500

    im actually stupid

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Robot.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import android.app.Activity;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpModeManagerNotifier;
 import com.qualcomm.robotcore.util.ThreadPool;
@@ -12,88 +14,90 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 
 public class Robot implements OpModeManagerNotifier.Notifications {
-  // Subsystems
-  public MecanumDrive drive;
-  public EncoderTest encoderTest;
-  // public Intake intake;
+    // Subsystems
+    public MecanumDrive drive;
+    public EncoderTest encoderTest;
+    // public Intake intake;
 
-  private List<Subsystem> subsystems;
-  private OpModeManagerImpl opModeManager;
-  private ExecutorService subsystemUpdateExecutor;
+    private List<Subsystem> subsystems;
+    private OpModeManagerImpl opModeManager;
+    private ExecutorService subsystemUpdateExecutor;
 
-  private boolean started;
-  private Telemetry telemetry;
+    private boolean started;
 
-  private OpMode opMode;
-
-  // Run the "update" function for every subsytem
-  private Runnable subsystemUpdateRunnable =
-      new Runnable() {
+    // Run the "update" function for every subsytem
+    private Runnable subsystemUpdateRunnable = new Runnable() {
         @Override
         public void run() {
-          while (!Thread.currentThread().isInterrupted()) {
-            for (Subsystem subsystem : subsystems) {
-              subsystem.update();
+            while (!Thread.currentThread().isInterrupted()) {
+                for (Subsystem subsystem : subsystems) {
+                    subsystem.update();
+                }
+
+                System.out.println("statut: " + Thread.currentThread().isInterrupted());
             }
-
-            System.out.println("statut: " + Thread.currentThread().isInterrupted());
-          }
         }
-      };
+    };
 
-  public Robot(OpMode opMode, Telemetry telemetry) {
-    subsystems = new ArrayList<>();
+    public Robot(OpMode opMode, Telemetry telemetry) {
+        subsystems = new ArrayList<>();
 
-    // Init subsystems
-    drive = new MecanumDrive(opMode.hardwareMap, telemetry);
-    subsystems.add(drive);
+        // Init subsystems
+        drive = new MecanumDrive(opMode.hardwareMap, telemetry);
+        subsystems.add(drive);
 
-    encoderTest = new EncoderTest(opMode.hardwareMap);
-    subsystems.add(encoderTest);
+        encoderTest = new EncoderTest(opMode.hardwareMap);
+        subsystems.add(encoderTest);
 
-    //    intake = new Intake(opMode.hardwareMap);
-    //    subsystems.add(encoderTest);
+        //    intake = new Intake(opMode.hardwareMap);
+        //    subsystems.add(encoderTest);
 
-    subsystemUpdateExecutor = ThreadPool.newSingleThreadExecutor("subsystem update");
-    this.telemetry = telemetry;
+        Activity activity = (Activity) opMode.hardwareMap.appContext;
+        opModeManager = OpModeManagerImpl.getOpModeManagerOfActivity(activity);
+        if (opModeManager != null) {
+            opModeManager.registerListener(this);
+        }
 
-    this.started = false;
-    this.opMode = opMode;
-  }
+        subsystemUpdateExecutor = ThreadPool.newSingleThreadExecutor("subsystem update");
 
-  // Starts subsystem executor
-  public void start() {
-    if (!started) {
-      subsystemUpdateExecutor.execute(subsystemUpdateRunnable);
-
-      started = true;
+        this.started = false;
     }
-  }
 
-  // Shuts down subsystem executor
-  public void stop() {
-    System.out.println("statut: STOPED THE ROBOT");
-    if (subsystemUpdateExecutor != null) {
-      subsystemUpdateExecutor.shutdownNow();
-      subsystemUpdateExecutor = null;
+    // Starts subsystem executor
+    public void start() {
+        if (!started) {
+            subsystemUpdateExecutor.execute(subsystemUpdateRunnable);
+
+            started = true;
+        }
     }
-  }
 
-  // ===============================================
-
-  @Override
-  public void onOpModePreInit(OpMode opMode) {}
-
-  @Override
-  public void onOpModePreStart(OpMode opMode) {}
-
-  @Override
-  public void onOpModePostStop(OpMode opMode) {
-    System.out.println("statut: STOPEED THE DAMN ROBOT");
-    stop();
-    if (opModeManager != null) {
-      opModeManager.unregisterListener(this);
-      opModeManager = null;
+    // Shuts down subsystem executor
+    public void stop() {
+        System.out.println("statut: STOPED THE ROBOT");
+        if (subsystemUpdateExecutor != null) {
+            subsystemUpdateExecutor.shutdownNow();
+            subsystemUpdateExecutor = null;
+        }
     }
-  }
+
+    // ===============================================
+
+    @Override
+    public void onOpModePreInit(OpMode opMode) {
+    }
+
+    @Override
+    public void onOpModePreStart(OpMode opMode) {
+    }
+
+    @Override
+    public void onOpModePostStop(OpMode opMode) {
+        System.out.println("statut: STOPEED THE DAMN ROBOT");
+        stop();
+        if (opModeManager != null) {
+            opModeManager.unregisterListener(this);
+            opModeManager = null;
+        }
+    }
 }
