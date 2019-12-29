e4a82b154eaa1ad20b8df534e63ebd08b7f8bc48
Havish Netla <netlahavish@gmail.com>
2019-12-29 11:31:42 -0500

    fixed it

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/path/Paths.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/path/Paths.kt
@@ -9,18 +9,23 @@ private var t = PathBuilder(Pose2d(20.32, 81.7, 0.0))
 data class PathPoint(val point: Vector2d, val followAngle: Double, val speed: Double, val turnSpeed: Double, val label: String)
 
 private var blockPositions: List<PathPoint> = listOf(
-        PathPoint(Vector2d(103.14, 23.668), 65.0, 0.3, 0.5, "first block"),
-        PathPoint(Vector2d(103.14, 43.985), 60.0, 0.3, 0.5, "second block"),
-        PathPoint(Vector2d(103.14, 64.305), 55.0, 0.3, 0.5, "third block"),
-        PathPoint(Vector2d(103.14, 84.625), 50.0, 0.3, 0.5, "fourth block"),
-        PathPoint(Vector2d(103.14, 104.945), 45.0, 0.3, 0.5, "fifth block"),
-        PathPoint(Vector2d(103.14, 125.265), 40.0, 0.3, 0.5, "sixth block")
+        PathPoint(Vector2d(103.14, 23.668), 90.0, 0.3, 0.5, "first block"),
+        PathPoint(Vector2d(103.14, 43.985), 90.0, 0.3, 0.5, "second block"),
+        PathPoint(Vector2d(103.14, 64.305), 90.0, 0.3, 0.5, "third block"),
+        PathPoint(Vector2d(103.14, 84.625), 90.0, 0.3, 0.5, "fourth block"),
+        PathPoint(Vector2d(103.14, 104.945), 90.0, 0.3, 0.5, "fifth block"),
+        PathPoint(Vector2d(103.14, 125.265), 90.0, 0.3, 0.5, "sixth block")
 )
 
 class Paths() {
     fun getPathToBlock(index: Int): ArrayList<PathSegment> {
         var path: ArrayList<PathSegment> = t
-                .addPoint(blockPositions[index].point,
+                .addPoint(Vector2d(blockPositions[index].point.x - 30, blockPositions[index].point.y),
+                        blockPositions[index].followAngle,
+                        blockPositions[index].speed,
+                        blockPositions[index].turnSpeed,
+                        blockPositions[index].label
+                ).addPoint(blockPositions[index].point,
                         blockPositions[index].followAngle,
                         blockPositions[index].speed,
                         blockPositions[index].turnSpeed,
