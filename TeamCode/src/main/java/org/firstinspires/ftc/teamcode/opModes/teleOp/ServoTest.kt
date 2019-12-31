7401c38efe41c512ad76459194d4706c7a259a78
Havish Netla <netlahavish@gmail.com>
2019-12-30 23:14:46 -0500

    f

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/teleOp/ServoTest.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/teleOp/ServoTest.kt
@@ -2,13 +2,16 @@ package org.firstinspires.ftc.teamcode.opModes.teleOp
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.Gamepad
 import org.firstinspires.ftc.teamcode.subsystems.Robot
+import org.firstinspires.ftc.teamcode.util.*
 
 @TeleOp(name = "Servo Test", group = "T")
 class ServoTest : OpMode() {
     private lateinit var robot: Robot
     private var pos = 0.5
     private var b = true
+    private var lastGamePad: Gamepad = Gamepad()
 
     override fun init() {
         robot = Robot(this, this.telemetry)
@@ -16,55 +19,44 @@ class ServoTest : OpMode() {
     }
 
     override fun loop() {
-        if (gamepad1.a) {
+        telemetry.addData("pos", pos)
+
+
+        if (gamepad2.a) {
             robot.lift.grap()
-        } else {
+        }
+
+        // ===============================
+        if (gamepad2.x) {
+            robot.lift.setLinkagePos(0.78)
+        }
+
+        //===============================
+        println("statut1: " + gamepad2.y    + ", " + lastGamePad.y)
+
+        val slowToggle = UtilToggle()    // Slows down drivetrain when on
+        if (gamepad2.y) {
+            println("statut1: got in here1")
             robot.lift.open()
         }
-
-        if (gamepad1.b) {
-            pos += 0.001
-        } else if (gamepad1.y) {
-            pos -= 0.001
-        }
-        telemetry.addData("pos",  pos)
-        robot.lift.setLinkagePos(pos)
-
-//        if(gamepad1.a) {
-//            robot.drive.grabFoundation()
-//        } else {
-//            robot.drive.openFoundationGrabber()
-//        }
-//        robot.lift.spinRotater(Lift.ROTATER_POSITION.RIGHT)
-
-//        robot.intake.power = gamepad1.left_stick_x.toDouble()
-//        telemetry.addData("intake", robot.intake.power)
-//        telemetry.addData("touchSensor", robot.drive.touchSensorState)
-
-//        if(gamepad1.a) {
-//            robot.drive.setRotaterPos(1.0)
-//        } else {
-//            robot.drive.setRotaterPos(0.5)
-//        }
-//
-//        if(gamepad1.b) {
-//            robot.drive.setGrabberPos(0.3)
-//        } else {
-//            robot.drive.setGrabberPos(0.6)
-//        }
-
-        if (gamepad1.a) {
-            robot.drive.grabBlock()
+        if (slowToggle.status(gamepad2.y) == UtilToggle.Status.COMPLETE) {
+            println("statut1: got in here")
+            robot.lift.setLinkagePos(0.36)
         }
 
-        if (gamepad1.b) {
-            robot.drive.releaseBlock()
+        //===============================
+        when {
+            gamepad1.right_trigger > 0.0 -> robot.intake.power = gamepad1.right_trigger.toDouble()
+            gamepad1.right_bumper -> robot.intake.power = -0.8
+            else -> robot.intake.power = 0.0
         }
 
-        if (gamepad1.y) {
-            robot.drive.stowBlock()
-        }
-        robot.intake.power = gamepad1.left_stick_x.toDouble()
 
+        robot.lift.lift(gamepad2.left_stick_y.toDouble())
+
+        telemetry.addData("touch sensor", robot.lift.touchSensorState)
+        telemetry.addData("lift encoder", robot.lift.encoderValue)
+
+        lastGamePad.copy(gamepad2)
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Lift.java
@@ -1,13 +1,18 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.util.PIDController;
 
 public class Lift extends Subsystem {
   private Telemetry telemetry;
+
+  private DigitalChannel touchSensor;
+
   private Servo linkage;
   private Servo rotater;
   private Servo grabber;
@@ -17,14 +22,23 @@ public class Lift extends Subsystem {
   private double linkagePos = 0.0;
   private double rotaterPos = 0.0;
   private double grabberPos = 0.0;
+  private double liftPower = 0.0;
 
   public Lift(HardwareMap map, Telemetry telemetry) {
     linkage = map.get(Servo.class, "LL");
     rotater = map.get(Servo.class, "LR");
     grabber = map.get(Servo.class, "LG");
 
-//    liftMotor = map.get(DcMotor.class, "LM");
+    liftMotor = map.get(DcMotor.class, "LM");
+
+    touchSensor = map.get(DigitalChannel.class, "LT");
     this.telemetry = telemetry;
+
+    liftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    // `liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    // liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    setRotaterPos(1.0);
+    setLinkagePos(0.36);
   }
 
   public void setLinkagePos(double pos) {
@@ -57,11 +71,42 @@ public class Lift extends Subsystem {
     }
   }
 
+  public void lift(double power) {
+    if (touchSensor.getState() && -power < 0.0) {
+      liftPower = 0.0;
+    } else {
+      liftPower = power;
+    }
+  }
+
+  public double liftToPos(double pos) {
+    PIDController pidController = new PIDController(0.1, 0, 0);
+
+    return pidController.getError(pos, -liftMotor.getCurrentPosition());
+  }
+
+  public boolean getTouchSensorState() {
+    return touchSensor.getState();
+  }
+
+  public double getEncoderValue() {
+    return liftMotor.getCurrentPosition();
+  }
+
   @Override
   public void update() {
     linkage.setPosition(linkagePos);
-    rotater.setPosition(rotaterPos);
+    // rotater.setPosition(rotaterPos);
     grabber.setPosition(grabberPos);
+
+    if (Math.abs(liftPower) < 0.001 && !touchSensor.getState()) {
+      if (Math.abs(liftMotor.getCurrentPosition()) < 540) {
+        liftPower = 0.1;
+      } else {
+        liftPower = 0.00021 * liftMotor.getCurrentPosition() - 0.01272065;
+      }
+    }
+    liftMotor.setPower(liftPower);
   }
 
   public enum ROTATER_POSITION {
